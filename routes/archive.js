a12a657e959522ca5200b9abbfeac51d4a4cf7cf
akasfei <akasfei@gmail.com>
2015-09-09 14:05:52 -0700

    API design

--- routes/archive.js
+++ routes/archive.js
@@ -2,8 +2,64 @@ module.exports = [
   {
     url: '/archive',
     method: 'get',
-    handler: function (req, res, srv) {
+    handler: function (req, res, srv, next) {
       // TODO: list blog articles
+    }
+  },
+  {
+    url: '/archive/:title',
+    method: 'get',
+    handler: function (req, res, srv, next) {
+      // TODO: retrieve an article
+    }
+  },
+  {
+    url: '/archive/:title',
+    method: 'post',
+    handler: function (req, res, srv, next) {
+      // TODO: add an article
+    }
+  },
+  {
+    url: '/archive/:title',
+    method: 'put',
+    handler: function (req, res, srv, next) {
+      // TODO: update an article
+    }
+  },
+  {
+    url: '/archive/:title',
+    method: 'delete',
+    handler: function (req, res, srv, next) {
+      // TODO: delete an article
+    }
+  },
+  {
+    url: '/archive/tag',
+    method: 'get',
+    handler: function (req, res, srv, next) {
+      // TODO: retrieve list of tags
+    }
+  },
+  {
+    url: '/archive/tag/:title',
+    method: 'post',
+    handler: function (req, res, srv, next) {
+      // TODO: create a tag
+    }
+  },
+  {
+    url: '/archive/tag/:title',
+    method: 'put',
+    handler: function (req, res, srv, next) {
+      // TODO: edit a tag
+    }
+  },
+  {
+    url: '/archive/tag/:title',
+    method: 'delete',
+    handler: function (req, res, srv, next) {
+      // TODO: delete a tag
     }
   }
 ]
--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,30 @@
 module.exports = [
   {
     url: '/archive/:title/comment',
+    method: 'get',
+    handler: function (req, res, srv, next) {
+      // TODO: retrieve comments
+    }
+  },
+  {
+    url: '/archive/:title/comment',
     method: 'post',
-    handler: function (req, res, srv) {
+    handler: function (req, res, srv, next) {
       // TODO: post new comment
+    }
+  },
+  {
+    url: '/archive/:title/comment/:id',
+    method: 'put',
+    handler: function (req, res, srv, next) {
+      // TODO: edit comment
+    }
+  },
+  {
+    url: '/archive/:title/comment/:id',
+    method: 'delete',
+    handler: function (req, res, srv, next) {
+      // TODO: delete a comment
     }
   }
 ]
